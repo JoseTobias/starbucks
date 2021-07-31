681fa75ae574c6096694161fb7e149bf4ae23b50
josetobias <tobiassouza0@gmail.com>
2021-07-31 14:36:57 -0300

    chore: cange api url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_BASE || 'http://localhost:5000'}/`,
+  baseURL: `${
+    process.env.REACT_APP_API_BASE ||
+    'https://610557584b92a000171c5eaa.mockapi.io'
+  }/`,
 });
 
 export default Api;
