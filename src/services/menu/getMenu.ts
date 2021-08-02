d539f22ef0f0cdef60bcb452d0699934c7fc789f
josetobias <tobiassouza0@gmail.com>
2021-08-01 22:03:39 -0300

    feat: add service to get menu

--- src/services/menu/getMenu.ts
+++ src/services/menu/getMenu.ts
@@ -1,1 +1,35 @@
-export {};
+import Api from 'config/api';
+import Endpoints from 'config/endpoints';
+
+export interface ErrorData {
+  message: string;
+  status: number;
+}
+
+export interface MenuItem {
+  name: string;
+  price: string;
+}
+
+interface Response {
+  storeName: string;
+  storeId: number;
+  list: Array<MenuItem>;
+}
+
+export const getMenuItens = (store: number): Promise<Response> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await Api.get<Response>(Endpoints.menu.get(store));
+      return resolve(data);
+    } catch (error) {
+      const response: ErrorData = { message: '', status: 500 };
+      if (error.response) {
+        response.message = error.response.statusText;
+        response.status = error.response.status;
+      }
+
+      return reject(response);
+    }
+  });
+};
